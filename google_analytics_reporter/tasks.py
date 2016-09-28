862cdfc982e1362c7a70797fb0c9146dd7a42746
Brandon Jurewicz <brandonjur@gmail.com>
2016-09-28 18:11:16 -0400

    py3 support

--- google_analytics_reporter/tasks.py
+++ google_analytics_reporter/tasks.py
@@ -1,11 +1,17 @@
-import httplib
-import urllib
-
+import sys
 from celery import shared_task
+
+if (sys.version_info > (3, 0)):
+    # Python 3
+    from http import client
+    from urllib.parse import urlencode
+else:
+    import httplib as client
+    from urllib import urlencode
 
 
 @shared_task
 def send_report_task(params_dict):
-    params = urllib.urlencode(params_dict)
-    connection = httplib.HTTPConnection('www.google-analytics.com')
+    params = urlencode(params_dict)
+    connection = client.HTTPConnection('www.google-analytics.com')
     return connection.request('POST', '/collect', params)
